9e8b6f44378cb77b77cfd567770cc0ca1c63f306
hjdtpz <hjdtpz@outlook.com>
2024-04-27 10:19:05 +0800

    premake

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+	staticruntime "off"
 
 	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
@@ -23,7 +24,7 @@ project "ImGui"
 	filter "system:windows"
         systemversion "latest"
         cppdialect "C++17"
-        staticruntime "On"
 
 	filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+	    runtime "Release"
+       
